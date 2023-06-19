ea6a3e33ebe94d252cfbf5cc8c33f405893344bd
Szymon Ludwiniak <259215@student.pwr.edu.pl>
2023-06-20 01:09:34 +0200

    dima pomusz

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,25 +1,27 @@
 class ReservationsController < ApplicationController
-    def index
-      @reservations = Reservation.all
-    end
-  
-    def new
-      @reservation = Reservation.new
-    end
-  
-    def create
-      @reservation = Reservation.new(reservation_params)
-      if @reservation.save!
-        redirect_to root_path, notice: "Rezerwacja została utworzona!"
-      else
-        render :new
-      end
-    end
-  
-    private
-  
-    def reservation_params
-      params.require(:reservation).permit(:reservationDate, :useDate, :desk_id, :user_id)
+
+  before_action :authenticate_user!, only: [ :edit, :update, :destroy]
+
+  def index
+    @reservations = Reservation.all
+  end
+
+  def new
+    @reservation = Reservation.new
+  end
+
+  def create
+    @reservation = Reservation.new(reservation_params)
+    if @reservation.save!
+      redirect_to root_path, notice: "Rezerwacja została utworzona!"
+    else
+      render :new
     end
   end
-  
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:reservationDate, :useDate, :desk_id, :user_id)
+  end
+end
