33b554f855b7f950fa6a826799152a4f5c17eb21
Szymon Ludwiniak <259215@student.pwr.edu.pl>
2023-06-20 02:20:27 +0200

    validate reservation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,4 +4,13 @@ class Reservation < ApplicationRecord
 
   validates :reservationDate, presence: true
   validates :useDate, presence: true
+  validate :unique_reservation_per_desk_and_date
+
+  private
+
+  def unique_reservation_per_desk_and_date
+    if Reservation.exists?(desk_id: desk_id, reservationDate: reservationDate)
+      errors.add(:base, "There is already a reservation for this desk on the selected date")
+    end
+  end
 end
